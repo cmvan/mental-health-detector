af4b3051a11ca991ea47b92d39ab1c3849f3c6fc
cmvan <cmvan@stanford.edu>
2025-02-25 01:18:21 -0800

    add scripts for preprocessing, splitting data

--- scripts/run_rope.bat
+++ scripts/run_rope.bat
@@ -1,23 +1,23 @@
 @echo off
 
 @REM Pretrain the model
-python src\run.py pretrain rope wiki.txt ^
+python src\run.py pretrain rope vocab.txt ^
         --writing_params_path rope.pretrain.params
         
 @REM @REM Finetune the model
-python src\run.py finetune rope wiki.txt ^
+python src\run.py finetune rope vocab.txt ^
         --reading_params_path rope.pretrain.params ^
         --writing_params_path rope.finetune.params ^
-        --finetune_corpus_path birth_places_train.tsv
+        --finetune_corpus_path crisis_train.tsv
         
 @REM Evaluate on the dev set; write to disk
-python src\run.py evaluate rope wiki.txt  ^
+python src\run.py evaluate rope vocab.txt  ^
         --reading_params_path rope.finetune.params ^
-        --eval_corpus_path birth_dev.tsv ^
+        --eval_corpus_path crisis_dev.tsv ^
         --outputs_path rope.pretrain.dev.predictions
         
 @REM Evaluate on the test set; write to disk
-python src\run.py evaluate rope wiki.txt  ^
+python src\run.py evaluate rope vocab.txt  ^
         --reading_params_path rope.finetune.params ^
-        --eval_corpus_path birth_test_inputs.tsv ^
+        --eval_corpus_path crisis_test.tsv ^
         --outputs_path rope.pretrain.test.predictions
--- scripts/run_rope.sh
+++ scripts/run_rope.sh
@@ -1,23 +1,23 @@
 ##! /bin/bash
 
 # Pretrain the model
-python src/run.py pretrain rope wiki.txt \
+python src/run.py pretrain rope vocab.txt \
         --writing_params_path rope.pretrain.params
         
 # Finetune the model
-python src/run.py finetune rope wiki.txt \
+python src/run.py finetune rope vocab.txt \
         --reading_params_path rope.pretrain.params \
         --writing_params_path rope.finetune.params \
-        --finetune_corpus_path birth_places_train.tsv
+        --finetune_corpus_path crisis_train.tsv
         
 # Evaluate on the dev set; write to disk
-python src/run.py evaluate rope wiki.txt  \
+python src/run.py evaluate rope vocab.txt  \
         --reading_params_path rope.finetune.params \
-        --eval_corpus_path birth_dev.tsv \
+        --eval_corpus_path crisis_dev.tsv \
         --outputs_path rope.pretrain.dev.predictions
         
 # Evaluate on the test set; write to disk
-python src/run.py evaluate rope wiki.txt  \
+python src/run.py evaluate rope vocab.txt  \
         --reading_params_path rope.finetune.params \
-        --eval_corpus_path birth_test_inputs.tsv \
+        --eval_corpus_path crisis_test.tsv \
         --outputs_path rope.pretrain.test.predictions
